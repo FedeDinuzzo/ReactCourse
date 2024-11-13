9cf6dfe8f2f899fd24442f22a247bbbdfa54e14c
FedeDinuzzo <68045599+FedeDinuzzo@users.noreply.github.com>
2024-11-13 00:24:56 -0300

    Clase 3
    Use State

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,27 @@ import { Button } from "./components";
 import "./App.css";
 
 function App() {
+  // Hooks gancho de react
+  // Utilizamos el useState para bindear el estado del componente y poder actualizarlo
   const [count, setCount] = useState(0);
+
   const countMore = () => {
-    setCount((count) => count + 1);
+    // Calculamos los incrementos en un solo `setTimeout`
+    let increment = 0;
+    const interval = setInterval(() => {
+      increment += 1;
+      setCount((count) => count + 1);
+
+      // Limpiamos el intervalo después de 3 incrementos
+      if (increment === 3) {
+        clearInterval(interval);
+      }
+    }, 500);
   };
+
   return (
     <>
+      {/* Para evitar que cada vez que hagas click se cree un metodo y se ejecute, pasamnos la referencia */}
       <Button label={`Count is ${count}`} parentMethod={countMore} />
     </>
   );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,3 @@
-import "./Button.css";
-
 interface Props {
   label: string;
   parentMethod: () => void;
@@ -8,7 +6,7 @@ export const Button = ({ label, parentMethod }: Props) => {
   return (
     // Emitimos un evento al padre
     // No lo ejecutamos: "parentMethod()" porque el padre lo va a manejar
-    <button className="custom-button" onClick={parentMethod}>
+    <button className="" onClick={parentMethod}>
       {label}
     </button>
   );
