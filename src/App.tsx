67798129ec15c8115618e73404033f59154f093a
FedeDinuzzo <68045599+FedeDinuzzo@users.noreply.github.com>
2024-11-27 00:59:05 -0300

    context fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
 import "./App.css";
-import { CompositionPatternButton, ColorRed, AppForm } from "./components";
-import { GlobalProvider } from "./context/global.context";
+import CustomForm from "./appClases/CustomForm";
 
 function App() {
-  // validaciones de form submit -> etc
-  const submit = () => {
-    console.log("Submitted");
-  };
-
-  const handleClick = () => {
-    console.log("Clickeado");
-  };
-
-  return (
-    <GlobalProvider>
-      <CompositionPatternButton parentMethod={handleClick}>
-        <ColorRed>My Label</ColorRed>
-        <AppForm>
-          <button type="submit" onClick={submit}></button>
-        </AppForm>
-      </CompositionPatternButton>
-    </GlobalProvider>
-  );
+  return <CustomForm />;
 }
 
 export default App;
